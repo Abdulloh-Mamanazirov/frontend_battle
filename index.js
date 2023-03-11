8e6f5d29aa4aa3c79c5c540acb6be089d1be8c75
muhammadqodir <dilnavo3@gmail.com>
2023-03-11 19:36:39 +0500

    portfolio da header footer form qilindi

---
+++ index.js
@@ -0,0 +1,8 @@
+const checkbox = document.getElementById("checkbox");
+const nav1 = document.getElementById("navbar");
+const nav2 = document.getElementById("nav2");
+checkbox.addEventListener("change", () => {
+    console.log("hello");
+  document.body.classList.toggle("dark");
+  navbar.classList.toggle("dark");
+  nav2.classList.toggle("dark");});
